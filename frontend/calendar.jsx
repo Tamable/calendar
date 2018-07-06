0ac0ee062c596fae280ca85aba68f1d3507fdec2
Tamae <t728532417114m@gmail.com>
2018-07-06 17:25:20 -0400

    bootstrapping current user

--- frontend/calendar.jsx
+++ frontend/calendar.jsx
@@ -9,23 +9,23 @@ import { login } from './actions/session_actions'
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
 
-  // let store;
-  // if (window.currentUser) {
-  //   const preloadedState = {
-  //     entities: {
-  //       users: { [window.currentUser.id]: window.currentUser }
-  //     },
-  //     session: { id: window.currentUser.id }
-  //   };
-    // store = configureStore(preloadedState);
-  //   delete window.currentUser;
-  // } else {
-  //   store = configureStore();
-  // }
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
 
 // TESTING START
 // ReactDOM.render(<h1>entry file</h1>, root);
-const store = configureStore();
+// const store = configureStore();
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   window.login = login
