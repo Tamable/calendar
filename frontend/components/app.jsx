94dcfa216f49e1ad492c4e51f86e334866983993
Tamae <t728532417114m@gmail.com>
2018-07-06 17:29:00 -0400

    auth route

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -20,8 +20,8 @@ const App = () => {
         <GreetingContainer />
       </header>
 
-      <Route path="/login" component={LogInFormContainer} />
-      <Route path="/signup" component={SignUpFormContainer} />
+      <AuthRoute exact path="/login" component={LogInFormContainer} />
+      <AuthRoute exact path="/signup" component={SignUpFormContainer} />
     </div>
   )
 };
---
+++ frontend/util/route_util.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect, withRouter } from 'react-router-dom';
+
+const mapStateToProps = (state) => {
+  return {
+    loggedIn: Boolean(state.session.id)
+  };
+};
+
+const Auth = ({ component: Component, path, loggedIn, exact }) => {
+  return (
+    <Route path={path} exact={exact} render={(props) => {
+      return(
+        !loggedIn ? (
+          <Component { ...props} />
+        ) : (
+          <Redirect to='/' />
+        )
+      )}} />
+  );
+};
+
+export const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth));
+
+const Protected = ({ component: Component, path, loggedIn, exact }) => (
+  <Route path={path} exact={exact} render={(props) => (
+     loggedIn ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to="/login" />
+    )
+  )} />
+);
+
+export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected));
